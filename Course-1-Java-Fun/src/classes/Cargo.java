2b98893cc2f99d768921c42393dba5c0f5c8082c
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-08-24 18:12:54 -0500

    Inheritence

--- Course-1-Java-Fun/src/classes/Cargo.java
+++ Course-1-Java-Fun/src/classes/Cargo.java
@@ -4,5 +4,32 @@ package classes;
  * Created by kartik on 8/24/16.
  */
 public class Cargo extends Flight {
-    
+
+    float maxCargoSpace = 1000.0f;
+    float usedCargoSpace;
+
+    public void addPackage(float h, float w, float d)
+    {
+        double size = h * w * d;
+        if (hasCargoSpace(size))
+        {
+            usedCargoSpace += size;
+        }
+        else
+        {
+            handlenospace();
+        }
+
+    }
+
+
+    private boolean hasCargoSpace(double size)
+    {
+        return usedCargoSpace + size <= maxCargoSpace;
+    }
+
+    private void handlenospace()
+    {
+        System.out.println("No space");
+    }
 }
