0457d37d0b6b00fc1431cb1ab202334bf5052e85
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-08-24 18:13:20 -0500

    Adding back main

--- Course-1-Java-Fun/src/classes/Flight.java
+++ Course-1-Java-Fun/src/classes/Flight.java
@@ -73,5 +73,36 @@ public class Flight {
 
         return jet;
     }
+
+    public static void main(String args[]) {
+        Flight emirates = new Flight();
+        emirates.addPassenger();
+        emirates.addPassenger();
+        Flight etihad = new Flight();
+        System.out.print(emirates.getSeats());
+
+
+        Flight airAsia = new Flight();
+        Flight airCanada = new Flight();
+
+        Flight qatar = null;
+
+        if (airAsia.hasRoom(airCanada))
+        {
+            qatar = airAsia.combineFlight(airCanada);
+        }
+
+        if (qatar!=null)
+        {
+            System.out.println("Combined");
+        }
+
+        // getters and setters
+
+        Flight cathy = new Flight();
+        cathy.setSeats(100);
+        System.out.println(cathy.getSeats());
+
+    }
 }
 
