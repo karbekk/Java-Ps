b18ee60161dd3bc78b104d07ca2f7b8dd1248150
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-08-24 18:12:32 -0500

    Main class

--- Course-1-Java-Fun/src/classes/Main.java
+++ Course-1-Java-Fun/src/classes/Main.java
@@ -1,37 +1,37 @@
-package classes;
-
-/**
- * Created by kartik on 8/24/16.
- */
-public class Main {
-    public static void main(String args[]) {
-        Flight emirates = new Flight();
-        emirates.addPassenger();
-        emirates.addPassenger();
-        Flight etihad = new Flight();
-        System.out.print(emirates.getSeats());
-
-
-        Flight airAsia = new Flight();
-        Flight airCanada = new Flight();
-
-        Flight qatar = null;
-
-        if (airAsia.hasRoom(airCanada))
-        {
-            qatar = airAsia.combineFlight(airCanada);
-        }
-
-        if (qatar!=null)
-        {
-            System.out.println("Combined");
-        }
-
-        // getters and setters
-
-        Flight cathy = new Flight();
-        cathy.setSeats(100);
-        System.out.println(cathy.getSeats());
-
-    }
-}
+//package classes;
+//
+///**
+// * Created by kartik on 8/24/16.
+// */
+//public class Main {
+//    public static void main(String args[]) {
+//        Flight emirates = new Flight();
+//        emirates.addPassenger();
+//        emirates.addPassenger();
+//        Flight etihad = new Flight();
+//        System.out.print(emirates.getSeats());
+//
+//
+//        Flight airAsia = new Flight();
+//        Flight airCanada = new Flight();
+//
+//        Flight qatar = null;
+//
+//        if (airAsia.hasRoom(airCanada))
+//        {
+//            qatar = airAsia.combineFlight(airCanada);
+//        }
+//
+//        if (qatar!=null)
+//        {
+//            System.out.println("Combined");
+//        }
+//
+//        // getters and setters
+//
+//        Flight cathy = new Flight();
+//        cathy.setSeats(100);
+//        System.out.println(cathy.getSeats());
+//
+//    }
+//}
