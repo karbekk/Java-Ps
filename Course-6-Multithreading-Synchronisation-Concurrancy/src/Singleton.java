905bfc9891b7f3696085098e3b51958681e97617
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-08-28 16:38:05 -0500

    Singleton class

--- Course-6-Multithreading-Synchronisation-Concurrancy/src/Singleton.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by kartik on 8/28/16.
- */
-public class Singleton {
-    public static void main(String[] args)
-    {
-        System.out.print("Singleton pattern");
-    }
-}
---
+++ Course-6-Multithreading-Synchronisation-Concurrancy/src/threading/Singleton.java
@@ -0,0 +1,26 @@
+package threading;
+
+/**
+ * Created by kartik on 8/28/16.
+ */
+public class Singleton {
+
+//    private static Singleton instance;
+//
+//    private Singleton()
+//    {
+//
+//    }
+//
+//    public static Singleton getInstance()
+//    {
+//        if (instance == null)
+//        {
+//            instance = new Singleton();
+//        }
+//        return instance;
+//
+//    }
+
+
+}
