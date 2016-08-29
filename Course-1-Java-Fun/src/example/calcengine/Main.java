2e95bc94b27e79ddaa10b83969da177ac15e1544
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-08-28 21:08:16 -0500

    Arrays & Loops & for each

--- Course-1-Java-Fun/src/example/calcengine/Main.java
+++ Course-1-Java-Fun/src/example/calcengine/Main.java
@@ -6,26 +6,27 @@ package example.calcengine;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
-        double val1 = 100.0d;
-        double val2 = 0.0d;
-        double result;
+    public static void main(String[] args) {
 
-        char opCode = 'd';
+        double[] leftvals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightvals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
+//
+//        double val1 = 100.0d;
+//        double val2 = 0.0d;
+//        double result;
+//
+//        char opCode = 'd';
 
-        if (opCode == 'a')
-        {
-            result = val1 + val2;
-        }
-        else if (opCode =='s')
-        {
-            result = val1 - val2;
-        }
-        else if (opCode =='d')
-        {
+        for (int i = 0; i < opCodes.length; i++) {
+            if (opCodes[i] == 'a') {
+                results[i] = leftvals[i] + rightvals[i];
+            } else if (opCodes[i] == 's') {
+                results[i] = leftvals[i] - rightvals[i];
+            } else if (opCodes[i] == 'd') {
 
-            result = val2!= 0.0d ? val1 / val2 : 0.0d;
+                results[i] = rightvals[i] != 0.0d ? leftvals[i] / rightvals[i] : 0.0d;
 //            if (val2!=0) {
 //                result = val1 / val2;
 //            }
@@ -33,20 +34,16 @@ public class Main {
 //            {
 //                result = 0.0d;
 //            }
+            } else if (opCodes[i] == 'm') {
+                results[i] = leftvals[i] * rightvals[i];
+            } else {
+                System.out.print("Error");
+                results[i] = 0.0d;
+            }
+
         }
-
-        else if (opCode =='s')
-        {
-            result = val1 * val2;
+        for (double theResult : results) {
+            System.out.println(theResult);
         }
-        else
-        {
-            System.out.print("Error");
-            result = 0.0d;
-        }
-
-        System.out.println(result);
-
-
     }
 }
