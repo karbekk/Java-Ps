8c3051c7c4edf6081c8bb9cbf3e95a92f673289c
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-08-28 19:19:09 -0500

    Baisc Program to test package

--- Course-1-Java-Fun/src/example/calcengine/Main.java
+++ Course-1-Java-Fun/src/example/calcengine/Main.java
@@ -9,10 +9,10 @@ public class Main {
     public static void main(String[] args)
     {
         double val1 = 100.0d;
-        double val2 = 50.0d;
+        double val2 = 0.0d;
         double result;
 
-        char opCode = 'a';
+        char opCode = 'd';
 
         if (opCode == 'a')
         {
@@ -24,7 +24,15 @@ public class Main {
         }
         else if (opCode =='d')
         {
-            result = val1 / val2;
+
+            result = val2!= 0.0d ? val1 / val2 : 0.0d;
+//            if (val2!=0) {
+//                result = val1 / val2;
+//            }
+//            else
+//            {
+//                result = 0.0d;
+//            }
         }
 
         else if (opCode =='s')
@@ -33,6 +41,7 @@ public class Main {
         }
         else
         {
+            System.out.print("Error");
             result = 0.0d;
         }
 
