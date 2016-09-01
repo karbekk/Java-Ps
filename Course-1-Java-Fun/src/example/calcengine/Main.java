59505cbc9dff89fa5d0c3eaed649757bb0bbf192
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-01 18:44:01 -0500

    Calling the overloaded method and cleaning the code in main method

--- Course-1-Java-Fun/src/example/calcengine/Main.java
+++ Course-1-Java-Fun/src/example/calcengine/Main.java
@@ -1,5 +1,7 @@
 package example.calcengine;
 
+
+
 /**
  * Created by kartik on 8/28/16.
  */
@@ -8,17 +10,38 @@ public class Main {
     public static void main(String[] args) {
 
         MathEquation[] equations = new MathEquation[4];
-        equations[0] = new MathEquation('d',100.0d,50.0d);
-        equations[1] = new MathEquation('a',25.0d,92.0d);
-        equations[2] = new MathEquation('s',225.0d,17.0d);
-        equations[3] = new MathEquation('m',11.0d,3.0d);
+        equations[0] = new MathEquation('d', 100.0d, 50.0d);
+        equations[1] = new MathEquation('a', 25.0d, 92.0d);
+        equations[2] = new MathEquation('s', 225.0d, 17.0d);
+        equations[3] = new MathEquation('m', 11.0d, 3.0d);
 
         for (MathEquation equation : equations) {
             equation.execute();
-                System.out.println("The result is: " + equation.getResult());
-            }
+            System.out.println("The result is: " + equation.getResult());
+
         }
 
+        System.out.println();
+        System.out.println("Overloading execution");
+        System.out.println();
+
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+
+        int leftInt = 9;
+        int rightInt = 4;
+
+        MathEquation equationoverload = new MathEquation('d');
+        equationoverload.execute(leftDouble,rightDouble);
+
+        System.out.print("The Result using double is :");
+        System.out.println(equationoverload.getResult());
+
+        equationoverload.execute();
+        System.out.println("The result using int is: "+equationoverload.getResult());
+
+
+    }
 
 //    public static MathEquation create(double leftVal, double rightVal, char opCode)
 //        {
@@ -29,5 +52,6 @@ public class Main {
 //        return equation;
 //
 //    }
-}
 
+
+}
