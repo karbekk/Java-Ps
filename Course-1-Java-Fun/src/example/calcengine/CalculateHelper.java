3a2cac2ed62a7d77ec85b89d893c2b56e0f2a914
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-02 14:49:24 -0500

    Overriding toString method and adding final static variables for diff symbols

--- Course-1-Java-Fun/src/example/calcengine/CalculateHelper.java
+++ Course-1-Java-Fun/src/example/calcengine/CalculateHelper.java
@@ -5,6 +5,12 @@ package example.calcengine;
  */
 public class CalculateHelper {
 
+
+    private static final char ADD_SYMBOL = '+';
+    private static final char SUBTRACT_SYMBOL = '+';
+    private static final char MULTIPLY_SYMBOL = '+';
+    private static final char DIVIDE_SYMBOL = '+';
+
     MathCommand command;
     double leftValue;
     double rightValue;
@@ -69,5 +75,42 @@ public class CalculateHelper {
         {
             command = MathCommand.Divide;
         }
+    }
+
+        @Override
+        public String toString()
+        {
+            char symbol = ' ';
+            switch (command)
+            {
+                case Add:
+                    symbol = ADD_SYMBOL;
+                    break;
+                case Subtract:
+                    symbol = SUBTRACT_SYMBOL;
+                    break;
+                case Multiply:
+                    symbol = MULTIPLY_SYMBOL;
+                    break;
+                case Divide:
+                    symbol = DIVIDE_SYMBOL;
+                    break;
+
+            }
+
+            StringBuilder sb = new StringBuilder(20);
+            sb.append(leftValue);
+            sb.append(' ');
+            sb.append(symbol);
+            sb.append(' ');
+            sb.append(rightValue);
+            sb.append(" = ");
+            sb.append(result);
+
+            return sb.to
+
+
+        }
+
     }
 }
