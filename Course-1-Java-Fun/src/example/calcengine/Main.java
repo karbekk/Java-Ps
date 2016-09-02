c81b9789d599a559831a43598d8b5b02b1d648da
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-01 22:17:11 -0500

    Instantiating all the child classes in main using the array of classes

--- Course-1-Java-Fun/src/example/calcengine/Main.java
+++ Course-1-Java-Fun/src/example/calcengine/Main.java
@@ -43,6 +43,24 @@ public class Main {
         equationoverload.execute((double)leftInt,rightInt);
         System.out.println("The result using best overloading is: "+equationoverload.getResult());
 
+        System.out.println();
+        System.out.println("Inheritance and over loading");
+        System.out.println();
+
+        // Instantiating the child classes through array of objects
+
+        CalculateBase[] calculators = {
+                new Divider(100.0d,50.0d),
+                new Adder(25.0d, 92.0d),
+                new Subtracter(225.0d,17.0d),
+                new Multiplier(11.0d,3.0d)
+        };
+
+        for (CalculateBase calculator:calculators)
+        {
+            calculator.calculate();
+            System.out.println("The result is: " + calculator.getResult());
+        }
 
     }
 
