9215f73fb213f4ec10f5e8699766eacccedc1675
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-01 20:53:50 -0500

    Adding child class adder to inherit from base class

---
+++ Course-1-Java-Fun/src/example/calcengine/Adder.java
@@ -0,0 +1,14 @@
+package example.calcengine;
+
+/**
+ * Created by kartik on 9/1/16.
+ */
+public class Adder extends CalculateBase{
+    public Adder(){}
+//    public Adder(double leftVal, double rightVal)
+//    {
+//        super(leftVal,rightVal);
+//    }
+
+    //public void calculate()
+}
