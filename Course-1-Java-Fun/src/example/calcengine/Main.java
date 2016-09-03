31cb0e7166c71fbc77805cb5f99caa5f96a17a99
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-02 20:52:21 -0500

    Adding invalid statements to handle errors

--- Course-1-Java-Fun/src/example/calcengine/Main.java
+++ Course-1-Java-Fun/src/example/calcengine/Main.java
@@ -1,7 +1,6 @@
 package example.calcengine;
 
 
-
 /**
  * Created by kartik on 8/28/16.
  */
@@ -10,6 +9,9 @@ public class Main {
     public static void main(String[] args) {
 
         String[] statements = {
+            "add 1.0",
+                "add xx 25.0",
+                "addX 0.0 0.0",
                 "divide 100.0 50.0",
                 "add 25.0 92.0",
                 "subtract 225.0 17.0",
@@ -20,8 +22,17 @@ public class Main {
         CalculateHelper helper = new CalculateHelper();
         for (String statement: statements)
         {
+            try{
             helper.process(statement);
             System.out.println(helper);
+            } catch (InvalidException e)
+            {
+                System.out.println(e.getMessage());
+                if(e.getCause() != null)
+                {
+                    System.out.println("Original Exception: "+ e.getCause().getMessage());
+                }
+            }
         }
 
     }
