b5e1f864ea47dc0148de69449fb63809c6106bf6
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-02 23:50:16 -0500

    Implementing interface methods in adder

--- Course-1-Java-Fun/src/example/calcengine/Adder.java
+++ Course-1-Java-Fun/src/example/calcengine/Adder.java
@@ -4,7 +4,7 @@ package example.calcengine;
  * Created by kartik on 9/1/16.
  */
 
-public class Adder extends CalculateBase{
+public class Adder extends CalculateBase implements MathProcessing{
     public Adder(){}
 
     // This method calls the constructor of the base class
@@ -20,5 +20,24 @@ public class Adder extends CalculateBase{
     {
         double value = getLeftVal() + getRightVal();
         setResult(value);
+    }
+
+    @Override
+    public String getKeyword() {
+        return "add";
+    }
+
+    @Override
+    public char getSymbol() {
+        return '+';
+    }
+
+    @Override
+    public double doCalculation(double leftVal, double rightVal) {
+
+        setLeftVal(leftVal);
+        setRightVal(rightVal);
+        calculate();
+        return getResult();
     }
 }
