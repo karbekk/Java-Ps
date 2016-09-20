2688b35374848a03e26e85331505ed2613afc97b
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-20 18:27:30 -0500

    Synchronised keys still producing Race condition with threads accessing a shared variable

--- Course-6-Multithreading-Synchronisation-Concurrancy/src/livecoding/module2/ProducerConsumer.java
+++ Course-6-Multithreading-Synchronisation-Concurrancy/src/livecoding/module2/ProducerConsumer.java
@@ -5,28 +5,35 @@ package livecoding.module2;
  */
 public class ProducerConsumer {
 
+    private static Object lock = new Object();
+
     private static int[] buffer;
     private static int count;
 
     static class Producer{
         void produce()
         {
+            synchronized (lock){
             while (isFull(buffer)){
 
+                }
+                buffer[count++]=1;
             }
-            buffer[count++]=1;
         }
     }
 
     static class Consumer{
         void consume()
         {
+            synchronized (lock){
             while(isEmpty(buffer)){
 
             }
             buffer[--count]=0;
         }
     }
+    }
+
 
     static boolean isFull(int[] buffer)
     {
