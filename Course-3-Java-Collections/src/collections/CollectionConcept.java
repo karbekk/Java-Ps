fa6ff51304112a931fa40a43f6f20d6af1b8adca
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-10-15 02:18:23 -0500

    Creating the Arraylist collection

---
+++ Course-3-Java-Collections/src/collections/CollectionConcept.java
@@ -0,0 +1,21 @@
+package collections;
+
+import collections.Product;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by kartik on 10/15/16.
+ */
+public class CollectionConcept {
+
+    public static void main(String[] args)
+    {
+        Product door = new Product("Wooden",32);
+        Product window = new Product("Glass",12);
+
+        Collection<Product> products = new ArrayList<>();
+    }
+
+}
