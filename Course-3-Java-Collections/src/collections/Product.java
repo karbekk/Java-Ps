567c26fd69af771450b387a4be0a89a8dea64ef7
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-10-15 04:36:43 -0500

    Complete product class for the working code

--- Course-3-Java-Collections/src/collections/Product.java
+++ Course-3-Java-Collections/src/collections/Product.java
@@ -1,32 +1,45 @@
 package collections;
 
+import java.util.Comparator;
+
 /**
  * Created by kartik on 10/15/16.
  */
+
 public class Product {
 
-    private String name;
-    private int weight;
+//	public static final Comparator<Product> BY_WEIGHT =
+//		new Comparator<Product>() {
+//			@Override
+//			public int compare(final Product p1, final Product p2) {
+//				return Integer.compare(p1.getWeight(), p2.getWeight());
+//		}
+//	};
 
+    public static final Comparator<Product> BY_WEIGHT =
+            Comparator.comparing(Product::getWeight);
+
+    private final String name;
+    private final int weight;
 
     public Product(String name, int weight) {
         this.name = name;
         this.weight = weight;
     }
 
-    public int getWeight() {
-        return weight;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getWeight() {
+        return weight;
     }
 
-    public void setWeight(int weight) {
-        this.weight = weight;
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + "'" +
+                ", weight=" + weight + '}';
     }
+
 }
