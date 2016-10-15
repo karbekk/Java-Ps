0c68b2e6b9bb10026c336a0b3792277c089c657e
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-10-15 02:35:32 -0500

    Add method on Arraylist and using the iterator to print each item

--- Course-3-Java-Collections/src/collections/CollectionConcept.java
+++ Course-3-Java-Collections/src/collections/CollectionConcept.java
@@ -4,6 +4,7 @@ import collections.Product;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * Created by kartik on 10/15/16.
@@ -14,8 +15,21 @@ public class CollectionConcept {
     {
         Product door = new Product("Wooden",32);
         Product window = new Product("Glass",12);
+        Product floor = new Product("panel",19);
 
         Collection<Product> products = new ArrayList<>();
+        products.add(door);
+        products.add(window);
+        products.add(floor);
+
+        System.out.print(products);
+
+        final Iterator<Product> productIterator =   products.iterator();
+        while (productIterator.hasNext())
+        {
+            Product product = productIterator.next();
+            System.out.println(product);
+        }
     }
 
 }
