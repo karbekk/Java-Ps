435e70542624c0071a0f63684376d8d2b73bc14c
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-10-17 00:04:45 -0500

    Complete product class for the working code

---
+++ Course-3-Java-Collections/src/collections/lists/ShipmentTest.java
@@ -0,0 +1,11 @@
+package collections.lists;
+
+import java.util.Collection;
+
+/**
+ * Created by kartik on 10/15/16.
+ */
+public class ShipmentTest {
+
+    private Shipment shipment = new ShipmentTest();
+}
