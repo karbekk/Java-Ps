7e48e58e97ec906d8f0c1d4372e86881c114bef0
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-10-21 23:38:52 -0500

    Adding few products

---
+++ Course-3-Java-Collections/src/collections/lists/ProductFixtures.java
@@ -0,0 +1,13 @@
+package collections.lists;
+
+import collections.Product;
+
+/**
+ * Created by kartik on 10/20/16.
+ */
+public class ProductFixtures {
+
+    public static Product door = new Product("Wodden Door",35);
+    public static Product floorPanel = new Product("Floor Panel",25);
+    public static Product window = new Product("Glass Window",10);
+}
