e84b22ca97144b5947c7991372adb4653f131fde
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-10-21 23:39:59 -0500

    Implementing shipment methods

--- Course-3-Java-Collections/src/collections/lists/ShipmentTest.java
+++ Course-3-Java-Collections/src/collections/lists/ShipmentTest.java
@@ -5,7 +5,15 @@ import java.util.Collection;
 /**
  * Created by kartik on 10/15/16.
  */
+
 public class ShipmentTest {
 
-    private Shipment shipment = new ShipmentTest();
+    private Shipment shipment = new Shipment();
+
+    public void shouldAddItems() throws Exception
+    {
+        shipment.add(door);
+        shipment.add(window);
+    }
+
 }
